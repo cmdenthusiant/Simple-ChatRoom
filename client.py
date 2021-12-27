015a7cc744a9c766640a9305e54078a0d1287f19
cmdenthusiant <53386624+cmdenthusiant@users.noreply.github.com>
2021-12-27 18:50:25 +0800

    Update client.py

--- client.py
+++ client.py
@@ -8,18 +8,16 @@ words = [
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
 ]
 
-serverIp = "59.149.49.218" #your Server Ip
+serverIp = "59.149.49.218"
 
 class chatClient:
     def __init__(self) -> None:
         self.username = ""
+        self.command = False
+        self.PingStartTime = 0
     def run(self):
-        port = self.login()
-        client = socket.socket()
-        client.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        client.bind(('0.0.0.0',port))
-        client.listen(20)
-        th = threading.Thread(target=self.listen,args=(client,))
+        client = self.login()
+        th = threading.Thread(target=self.recv,args=(client,))
         th.daemon = True
         th.start()
         while True:
@@ -30,16 +28,10 @@ class chatClient:
                     self.help()
                     continue
                 if text.startswith("/"):
-                    self.send(text,True)
+                    self.send(client,text,True)
                     continue
-                self.send(text)
+                self.send(client,text)
             except KeyboardInterrupt:sys.exit(0)
-    def listen(self,client):
-        while True:
-            conn = client.accept()[0]
-            th = threading.Thread(target=self.recv,args=(conn,))
-            th.daemon = True
-            th.start()
     def login(self):
         s = socket.socket()
         print("Connecting to Server...")
@@ -72,15 +64,9 @@ class chatClient:
             time.sleep(2)
             sys.exit(0)
         data = s.recv(1024)
-        if data.decode().startswith("Logined"):
-            try:
-                port = int(data.decode().split("Logined,port:")[1])
-                print("[*console*]Your are in the Chat now! Type /help for commands\n")
-                return port
-            except:
-                print("Something went wrong...")
-                time.sleep(2)
-                sys.exit(0)
+        if data.decode() == "Logined":
+            print("[*console*]Your are in the Chat now! Type /help for commands\n")
+            return s
         else:
             print("Something went wrong...")
             time.sleep(2)
@@ -89,46 +75,40 @@ class chatClient:
         key = ""
         for i in range(random.randint(1,15)):key += words[random.randint(0,len(words)-1)]
         return key
-    def recv(self,conn):
-        try:
-            bdata = conn.recv(1024)
-            data = bdata.decode()
-            text = dec(data.split(" ")[1],data.split(" ")[0])
-            objs = json.loads(text)
-            sendtext = objs['text']
-            fromUser = objs['user']
-        except:
-            conn.close()
-            return
-        print("["+fromUser+"]: "+sendtext)
-        conn.close()
-    def send(self,text,command=False):
+    def recv(self,client):
+        while True:
+            try:
+                bdata = client.recv(1024)
+                data = bdata.decode()
+                text = dec(data.split(" ")[1],data.split(" ")[0])
+                objs = json.loads(text)
+                sendtext = objs['text']
+                fromUser = objs['user']
+                if self.command:
+                    if sendtext == "Ping:":
+                        sendtext += str(round((time.time()-self.PingStartTime)*1000))+"ms"
+                    self.command = False
+            except:
+                continue
+            print("["+fromUser+"]: "+sendtext)
+            continue
+    def send(self,client,text,command=False):
         if command:
+            self.command = True
             if text == "/ping":
                 startTime = time.time()
-        s = socket.socket()
-        try:
-            s.connect((serverIp,5007)) 
-        except:
-            print("Server offline")
-            time.sleep(2)
-            sys.exit(0)
+                self.PingStartTime = startTime
         key = self.randomKey()
-        sended = s.send(str.encode(key+" "+enc('{"user":"'+self.username+'","text":"'+text+'"}', key)))
+        try:sended = client.send(str.encode(key+" "+enc('{"user":"'+self.username+'","text":"'+text+'"}', key)))
+        except:
+            print("Disconnected!\nRetrying...")
+            self.run()
+            return
         if not sended:
             print("Something went wrong...")
             time.sleep(2)
             sys.exit(0)
-        if command:
-            try:data = s.recv(1024).decode()
-            except:
-                s.close()
-                return
-            if data == "Ping:":
-                data += str(round((time.time()-startTime)*1000))+"ms"
-            if data != "":
-                print("[*console*]: "+data)
-        s.close()
+        return
     def help(self):
         print("="*50+"\n/online :get online members\n/ping :get ping(ms)\n"+"="*50)
 
